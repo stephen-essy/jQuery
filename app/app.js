ce4db15fb1e38248563053d2042f223c8ccec94c
Stephen-essy <stephenesau02@gmail.com>
2025-06-23 19:23:56 +0300

    jQuery intergration

---
+++ app/app.js
@@ -0,0 +1,8 @@
+$(document).ready(()=>{
+    $(".hero-main-heading").text("jQuery").css({
+        "font-size":"40pt",
+        "font-family":"poppins",
+        "font-weight":"600",
+        "text-transform":"upper-case"
+    })
+})
